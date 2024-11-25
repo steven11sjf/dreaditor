7b2999805041378051d2257e2785c194679287aa
Steven Franklin <steve11sjf@gmail.com>
2024-11-24 18:15:29 -0600

    bmsnav support
    - Map geometry is now painted as background
    - Only paints geo in the viewport to reduce number of draws required
    - rectangles are faster than polys
    - depends on MEDS changes

--- src/dreaditor/widgets/scenario_viewer.py
+++ src/dreaditor/widgets/scenario_viewer.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 import logging
 from typing import TYPE_CHECKING
 
-from PySide6.QtCore import QPointF, QRectF, Qt
-from PySide6.QtGui import QBrush, QColor, QPen, QWheelEvent
-from PySide6.QtWidgets import QGraphicsPixmapItem, QGraphicsView
+from PySide6.QtCore import QPointF, QRect, QRectF, Qt
+from PySide6.QtGui import QBrush, QColor, QMouseEvent, QPainter, QPen, QWheelEvent
+from PySide6.QtWidgets import QGraphicsView
 
 from dreaditor.widgets.collision_camera_item import CollisionCameraItem
 from dreaditor.widgets.map_geometry import MapGeometry
@@ -28,19 +28,20 @@ COLLISION_CAM_COLOR = QColor(255, 200, 255, 255)
 
 class ScenarioViewer(QGraphicsView):
     rom_manager: RomManager
-    mapitem: QGraphicsPixmapItem
+    map_geometry: MapGeometry
 
     def __init__(self, scene: ScenarioScene, rom_manager: RomManager):
         super().__init__(scene)
         self.logger = logging.getLogger(type(self).__name__)
         self.logger.info("Initialized ScenarioViewer!")
         self.rom_manager = rom_manager
+        self.map_geometry = None
         self.setScene(scene)
         self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.setBackgroundBrush(BACKGROUND)
-        self.setViewportUpdateMode(self.ViewportUpdateMode.FullViewportUpdate)
+        self.setViewportUpdateMode(self.ViewportUpdateMode.SmartViewportUpdate)
         self.setTransformationAnchor(self.ViewportAnchor.AnchorUnderMouse)
 
     def on_new_scenario_selected(self, scenario: Scenario):
@@ -51,9 +52,8 @@ class ScenarioViewer(QGraphicsView):
         self.scene().addItem(actor.actor_dot)
         actor.actor_dot.assign_painter_widgets()
 
-    def add_map_geo(self, verts: list[list[float]], indices: list[int], color: QColor | None, z: float):
-        geo = MapGeometry(verts, indices, color, z, None)
-        self.scene().addItem(geo)
+    def set_map_geo(self, verts: list[list[float]], areas: dict, color: QColor | None, z: float):
+        self.map_geometry = MapGeometry(verts, areas, color, z)
 
     def add_collision_camera(self, cc: dict) -> CollisionCameraItem:
         res = CollisionCameraItem(cc)
@@ -73,3 +73,13 @@ class ScenarioViewer(QGraphicsView):
     def wheelEvent(self, event: QWheelEvent | None) -> None:
         zoomFactor = ZOOM_FACTOR if event.angleDelta().y() > 0 else 1 / ZOOM_FACTOR
         self.scale(zoomFactor, zoomFactor)
+
+    def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        pos = self.mapToScene(event.pos())
+        self.logger.debug(f"Mouse x={pos.x()} y={-pos.y()}")
+        return super().mouseMoveEvent(event)
+
+    def drawBackground(self, painter: QPainter, rect: QRectF | QRect) -> None:
+        super().drawBackground(painter, rect)
+        if self.map_geometry:
+            self.map_geometry.paint_as_background(painter, rect)
